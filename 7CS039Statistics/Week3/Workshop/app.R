e0567d0b88082294dda9d3ad4721ec5701455807
maz786 <maz786@outlook.com>
2024-02-11 23:43:34 +0000

    Add files via upload

---
+++ 7CS039Statistics/Week3/Workshop/app.R
@@ -0,0 +1,54 @@
+library(shiny)
+
+# Define UI for app 
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Exploring the Bodyfat data!"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+      
+    )
+  )
+  
+  
+)
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+
+  output$distPlot <- renderPlot({
+    Bodyfat <- read.csv("~/Desktop/WlvTeaching/2021_22/Cross_Sem/7CS039 Stats/Labs/Lab_3_Corr_Reg/Bodyfat.csv")
+    x    <- Bodyfat$Weight
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlab = "Whatever the x is",
+         main = "Histogram of Whatever this is")
+    
+  })
+  
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
